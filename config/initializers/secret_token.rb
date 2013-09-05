f382360622e0d9688b34aea08b5b539f1a925966
hc0519 <reversal623@gmail.com>
2013-09-06 03:34:53 +0900

    Initial commit for twitter stream test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterStream::Application.config.secret_key_base = '56f05c9320fc08d56a674a14ed266aaa3078d672d32a4d440c5b5ef80d8fd819cf179dea2895e34e84f387c75e941f4d89def4a46a21bf6257ffe42f90bf1828'
